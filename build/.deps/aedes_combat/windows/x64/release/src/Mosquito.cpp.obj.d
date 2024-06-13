9bdb6e9999f83924814f9a6c5a406829ae2d145f
LucasEmmanuelRM <lucas.emmanuel10@hotmail.com>
2024-06-13 02:22:39 -0300

    teste

--- build/.deps/aedes_combat/windows/x64/release/src/Mosquito.cpp.obj.d
+++ build/.deps/aedes_combat/windows/x64/release/src/Mosquito.cpp.obj.d
@@ -1,29 +1,4 @@
 {
-    files = {
-        [[src\Mosquito.cpp]]
-    },
-    values = {
-        [[C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64\cl.exe]],
-        {
-            "-nologo",
-            "-Iinclude",
-            "-DFREEGLUT_STATIC=1",
-            "-DFREEGLUT_LIB_PRAGMAS=0",
-            "-DGLEW_NO_GLU",
-            "-DGLEW_STATIC",
-            "/EHsc",
-            "-external:W0",
-            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\f\freeglut\v3.4.0\9e1e9a63d6b948868b158c0a7189e4ed\include]],
-            "-external:W0",
-            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\s\stb\2023.12.15\1e9afa0be82a4afeaeaa89283f7b4e4d\include]],
-            "-external:W0",
-            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\s\stb\2023.12.15\1e9afa0be82a4afeaeaa89283f7b4e4d\include\stb]],
-            "-external:W0",
-            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\m\miniaudio\0.11.21\08eaa467bf644effb19c903e8c4ac2d4\include]],
-            "-external:W0",
-            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\g\glew\2.2.0\6a8fc1e310a44318beb712a418386242\include]]
-        }
-    },
     depfiles_cl_json = "{\
     \"Version\": \"1.1\",\
     \"Data\": {\
@@ -251,5 +226,30 @@
         \"ImportedModules\": [],\
         \"ImportedHeaderUnits\": []\
     }\
-}"
+}",
+    files = {
+        [[src\Mosquito.cpp]]
+    },
+    values = {
+        [[C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64\cl.exe]],
+        {
+            "-nologo",
+            "-Iinclude",
+            "-DFREEGLUT_STATIC=1",
+            "-DFREEGLUT_LIB_PRAGMAS=0",
+            "-DGLEW_NO_GLU",
+            "-DGLEW_STATIC",
+            "/EHsc",
+            "-external:W0",
+            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\f\freeglut\v3.4.0\9e1e9a63d6b948868b158c0a7189e4ed\include]],
+            "-external:W0",
+            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\s\stb\2023.12.15\1e9afa0be82a4afeaeaa89283f7b4e4d\include]],
+            "-external:W0",
+            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\s\stb\2023.12.15\1e9afa0be82a4afeaeaa89283f7b4e4d\include\stb]],
+            "-external:W0",
+            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\m\miniaudio\0.11.21\08eaa467bf644effb19c903e8c4ac2d4\include]],
+            "-external:W0",
+            [[-external:IC:\Users\Lucas\AppData\Local\.xmake\packages\g\glew\2.2.0\6a8fc1e310a44318beb712a418386242\include]]
+        }
+    }
 }
